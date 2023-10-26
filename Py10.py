15041c98cca9fa187658e393f9fc7b841620094a
Nikita <nikitabogachev@rambler.ru>
2023-10-26 19:33:17 +0300

    exam finished

--- Py10.py
+++ Py10.py
@@ -1,8 +1,14 @@
 #  Файл с заданием создан
 
 import random
+import pandas as pd
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
 data.head()
+
+data.loc[data['whoAmI'] == 'robot', 'robot'] = 1
+data.loc[data['whoAmI'] == 'human', 'robot'] = 0
+data.loc[data['whoAmI'] == 'human', 'human'] = 1
+data.loc[data['whoAmI'] == 'robot', 'human'] = 0
